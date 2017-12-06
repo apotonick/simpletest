dc04903d8f514f85468f9427993eecb6653c679c
Nick Sutterer <apotonick@gmail.com>
2017-12-06 08:31:43 +0100

    each #test is a Group with childs, the can only be called and will return their results.

--- lib/simpletest.rb
+++ lib/simpletest.rb
@@ -9,21 +9,51 @@ end
 require "minitest/spec"
 
 class Simpletest
-  include Minitest::Assertions
-  def assertions
-    @assertions_counter
-  end
-  def assertions=(i)
-    @assertions_counter = i
+  # Instance where the assertions for one Simpletest "class" are run.
+  class Test
+    include Minitest::Assertions
+    def assertions
+      @assertions_counter
+    end
+    def assertions=(i)
+      @assertions_counter = i
+    end
   end
 
   def initialize(&block)
     @assertions_counter = 0
-
-    instance_exec(&block)
   end
 
-  def test(what="", &block)
-    block.call
+  class Group
+    def initialize(description, block)
+      @description, @block = description, block
+      @childs = []
+    end
+
+    def test(description="implement me!", &block) # DSL
+      @childs += [Group.new(description, block)]
+    end
+
+    def run_childs
+      @childs.collect { |group| group.() }
+    end
+
+    def call
+      instance_exec(&@block) # todo: call
+      results = run_childs
+
+      [@assertions, *results]
+    end
+
+    # FIXME: where does the assertion backend come from?
+    def assert_equal(*)
+      @assertions ||=[]
+      @assertions << @description
+    end
+  end
+
+  def self.test(description, &block)
+    results = Group.new(description, block).()
+    pp results
   end
 end
--- test/simpletest_test.rb
+++ test/simpletest_test.rb
@@ -12,30 +12,71 @@ class SimpletestTest < Minitest::Spec
   # end
 
   it do
+    Simpletest.test "bla" do #|*|
+      struct = Struct.new(:a).new
+      # assert_equal 11
 
-    t = Simpletest do
-      test "Struct accessors" do
-        struct = Struct.new(:a).new
+      test { |struct| assert_equal Struct, struct }
 
-        test { assert_kind_of Struct, struct }
+      test "calling writers mutates" do #|struct|
+        struct[:a] = 1
 
-        test "calling writers mutates" do
-          struct[:a] = 1
+        assert_equal( {a: 1},  struct.to_h)
+        assert_equal( [[:a,1]], struct.to_a)
+      end
 
-          assert_equal( {a: 1}, struct.to_h)
-        end
-
-        test "reader returns value" do
-          assert_equal 2, struct[:a]
-        end
+      test "reader returns value" do |struct|
+        assert_equal 2#, struct[:a]
       end
     end
-
-
   end
+
+  # it do
+
+  #   t = Simpletest do
+  #     test "Struct accessors" do
+  #       struct = Struct.new(:a).new
+  #       # let(:struct) { Struct.new(:a).new }
+
+  #       test { |struct| assert_kind_of Struct, struct }
+
+  #       test "calling writers mutates" do |struct|
+  #         struct[:a] = 1
+
+  #         assert_equal( {a: 1}, struct.to_h)
+  #       end
+
+  #       test "reader returns value" do |struct|
+  #         assert_equal 2, struct[:a]
+  #       end
+  #     end
+  #   end
+
+  #   assert false
+
+
+  # end
 
 end
 
 
 # 18 failures when only one thing in let didn't work
-# i don't want to repeat the first #test in #before just to satisfy the framework. it's not DRY
+# i want the order of the tests, because it's a scenario or story to tell - and massively helps when designing APIs. also, reading.
+# i don't want to repeat the first #test setup (eg `struct = Struct.new(:a).new`) in #before just to satisfy the framework. it's not DRY
+# the more indentations, the harder to read
+
+
+=begin
+ the idea in Minitest is that each it block is a method `MyTest#test_01`
+ this is then run in Minitest#run_one_method with the following invocation
+   result = MyTest.new(:test_01).run
+  where only that one test method is run. why not a callable object per test case? this would also allow better access control of what to pass in.
+ the result is then
+  #<SimpletestTest:0x0000000179d988
+   @NAME="test_0001_anonymous",
+   @assertions=1,
+   @failures=[#<Minitest::Assertion: Expected false to be truthy.>],
+   @time=3.4210970625281334e-05>
+
+
+=end
